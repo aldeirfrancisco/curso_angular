3a70d7e9115b6788dbca480605569b1276343d19
aldeir <dide123francisco@gmail.com>
2022-12-19 18:11:44 -0300

    trabalhando com class e style binding

--- introducao/inicio_angular/src/app/app.module.ts
+++ introducao/inicio_angular/src/app/app.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+
 import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,8 @@ import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
     MeuPrimeiroComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
