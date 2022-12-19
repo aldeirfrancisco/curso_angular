ab738c9f70dadf6b7bb3c838e7d980f3555bc67b
aldeir <dide123francisco@gmail.com>
2022-12-19 15:19:12 -0300

    limpando o arquivo

--- introducao/inicio_angular/src/app/cursos/cursos.component.ts
+++ introducao/inicio_angular/src/app/cursos/cursos.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { CursosService } from './cursos.service';
 
 @Component({
   selector: 'app-cursos',
@@ -8,11 +7,11 @@ import { CursosService } from './cursos.service';
 })
 export class CursosComponent {
 
-  cursos: string[]=  [];
+ 
 
-  constructor(private cursoSercive: CursosService) { 
+  constructor() { 
 
-    this.cursos = this.cursoSercive.getCursos();
+ 
   }
 
 
