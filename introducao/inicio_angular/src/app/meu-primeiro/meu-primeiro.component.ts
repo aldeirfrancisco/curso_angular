e0b6e827b64580e0fb7d9a7213a1140bed8bc7de
aldeir <dide123francisco@gmail.com>
2022-12-19 19:24:12 -0300

    ajuste

--- introducao/inicio_angular/src/app/meu-primeiro/meu-primeiro.component.ts
+++ introducao/inicio_angular/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -2,7 +2,8 @@ import { Component} from "@angular/core";
 
 @Component({
     selector: 'meu-primeiro-component', 
-    templateUrl: './meu-primeiro.component.html'
+    templateUrl: './meu-primeiro.component.html',
+    styleUrls: ['./meu-primeiro.component.html.css']
     
 })
 export class MeuPrimeiroComponent {
