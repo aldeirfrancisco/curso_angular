605b1baeaddb4acd85fb437876d1409564ed4eed
aldeir <dide123francisco@gmail.com>
2022-12-20 14:26:54 -0300

    input properties

--- introducao/inicio_angular/src/app/input-output-properties/input-output-properties.component.ts
+++ introducao/inicio_angular/src/app/input-output-properties/input-output-properties.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-input-output-properties',
   templateUrl: './input-output-properties.component.html',
-  styleUrls: ['./input-output-properties.component.css']
+  styleUrls: ['./input-output-properties.component.css'],
+  inputs: ['aqui']
 })
-export class InputOutputPropertiesComponent {
+export class InputOutputPropertiesComponent  implements OnInit{
+  //pode ser desse dessa 3 formas
+  @Input('vamos') nomeCurso: string = 'a'; 
+  @Input() nome: string = 'a';
+  aqui: string = '';
 
+
+  constructor(){}
+  
+    ngOnInit(): void {     
+    }
 }
--- introducao/inicio_angular/src/app/meu-primeiro/meu-primeiro.component.ts
+++ introducao/inicio_angular/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -1,4 +1,4 @@
-import { Component} from "@angular/core";
+import { Component, OnInit} from "@angular/core";
 
 @Component({
     selector: 'meu-primeiro-component', 
@@ -6,6 +6,14 @@ import { Component} from "@angular/core";
     styleUrls: ['./meu-primeiro.component.html.css']
     
 })
-export class MeuPrimeiroComponent {
+export class MeuPrimeiroComponent  implements OnInit{
 
+    nomeCurso: string = ' 1 curso silva';
+    nome: string = '2 silva';
+    aqui: string = '3 silva';
+
+
+    constructor(){}
+    ngOnInit(): void {  
+    }
 }
