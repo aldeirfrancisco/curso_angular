81f0e191574803008e4b2a44b20a35a6508d0dbd
aldeir <dide123francisco@gmail.com>
2023-01-24 21:21:35 -0300

    criando diretivas customizadas com ElementRef e Renderer2

--- introducao/inicio_angular/src/app/app.module.ts
+++ introducao/inicio_angular/src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
 import { InputOutputPropertiesComponent } from './input-output-properties/input-output-properties.component';
 import { OutputPropertiesComponent } from './output-properties/output-properties.component';
+import { FundoAmareloDirective } from './shared/fundo-amarelo.directive';
+import { DiretivasCustomizadasComponent } from './diretivas-customizadas/diretivas-customizadas.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MeuPrimeiroComponent,
     InputOutputPropertiesComponent,
-    OutputPropertiesComponent
+    OutputPropertiesComponent,
+    FundoAmareloDirective,
+    DiretivasCustomizadasComponent
   ],
   imports: [
     BrowserModule
