5afee8534999090adbf625596622ab865c312c0c
aldeir <dide123francisco@gmail.com>
2023-01-24 22:16:28 -0300

    código refatorado

--- introducao/inicio_angular/src/app/shared/highlight-mouse.directive.ts
+++ introducao/inicio_angular/src/app/shared/highlight-mouse.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef,  HostListener, Renderer2 } from '@angular/core';
+import { Directive, ElementRef,  HostBinding,  HostListener, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[mouse]'
@@ -7,17 +7,26 @@ export class HighlightMouseDirective {
  
 
   @HostListener('mouseenter') onMouseOver(){
-    this._renderer2.setStyle(this._elementRef.nativeElement, 
-      "background-color",
-      'yellow')
+    // this._renderer2.setStyle(this._elementRef.nativeElement, 
+    //   "background-color",
+    //   'yellow')
+    this.backgroundColor = 'yellow';
   }
+
   @HostListener('mouseleave') onMouseleave(){
-    this._renderer2.setStyle(this._elementRef.nativeElement, 
-      "background-color",
-      'white')
+    // this._renderer2.setStyle(this._elementRef.nativeElement, 
+    //   "background-color",
+    //   'white')
+    this.backgroundColor = 'white';
   }
-  constructor( private _elementRef: ElementRef,
-    private _renderer2: Renderer2 ) {
+   //HostBinding metadado permiti que faça a 
+   //ligaçao de um atributo da diretiva com um atributo do htm
+  @HostBinding('style.backgroundColor') backgroundColor: string;
+
+  constructor( 
+    // private _elementRef: ElementRef,
+    // private _renderer2: Renderer2
+     ) {
 
       
      }
