a3b5909e1b29c02cfe230fbf0ab83f999dd86553
aldeir <dide123francisco@gmail.com>
2023-02-08 17:39:13 -0300

    ajsute route

--- introducao/rotas/src/app/app-routing.module.ts
+++ introducao/rotas/src/app/app-routing.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 import { CursosComponent } from './cursos/cursos.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { NavComponent } from './nav/nav.component';
 
 const routes: Routes = [
-  {path:'login',component: LoginComponent},
-  {path:'home',component: HomeComponent},
-  {path:'Curso',component: CursosComponent},
-  {path:'',component: HomeComponent}
+
+  {
+    path: '' , component: NavComponent, children:[
+      {path:'home',component: HomeComponent},
+      {path:'login',component: LoginComponent},
+      {path:'curso',component: CursosComponent},
+      
+    ]
+  }
+  
 ];
 
 @NgModule({
--- introducao/rotas/src/app/app.module.ts
+++ introducao/rotas/src/app/app.module.ts
@@ -12,13 +12,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { CursosComponent } from './cursos/cursos.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LoginComponent,
-    CursosComponent
+    CursosComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
