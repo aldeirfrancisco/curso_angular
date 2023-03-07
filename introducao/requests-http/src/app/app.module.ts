e60dae185067bfaec0102cfc680d13098e84cd71
aldeir <dide123francisco@gmail.com>
2023-03-07 18:21:07 -0300

    install bootstrap  e configurando rotas

--- introducao/requests-http/src/app/app.module.ts
+++ introducao/requests-http/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CursosListaComponent } from './cursos/cursos-lista/cursos-lista.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CursosListaComponent
   ],
   imports: [
     BrowserModule,
