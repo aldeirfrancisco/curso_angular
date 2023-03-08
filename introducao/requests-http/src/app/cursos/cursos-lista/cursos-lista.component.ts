bd5a00a020998274e82d022f1c2fe506ba6f7fcf
aldeir <dide123francisco@gmail.com>
2023-03-08 16:44:14 -0300

    maneira de captura erros ( +  erro com async)

--- introducao/requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ introducao/requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, take, catchError, of, Subject } from 'rxjs';
 import { Curso } from '../curso';
 import { CursosService } from '../cursos.service';
 
@@ -14,7 +14,7 @@ export class CursosListaComponent implements OnInit {
   //cursos: Curso[] = [];
   //$ variavel com isso é um observable
   cursos$: Observable<Curso[]> = new Observable();
-
+  error$ =  new Subject<boolean>();
   constructor(private cursoService: CursosService){}
 
   //observable são lazy (preguiçoso)
@@ -23,7 +23,31 @@ export class CursosListaComponent implements OnInit {
   //subscribe ativa a inscrição
   ngOnInit(){
 
+
     //this.cursoService.list().subscribe( cursos => this.cursos = cursos)
+
+    this.onRefresh();
+
+    // const aqui  = this.cursoService.list()
+    //                    .subscribe( dados => {
+    //                      console.log(dados);
+    //                    }, error => {
+    //                        console.error(error)
+    //                    }, () => {
+    //                     console.log('Observable completo')
+    //                    });
+  }
+
+  onRefresh() {
     this.cursos$ = this.cursoService.list()
+    .pipe(
+        catchError( error =>  {
+          console.error("error ",error);
+          this.error$.next(true);
+          return of();
+        }),
+        take(1)
+     );
+
   }
 }
